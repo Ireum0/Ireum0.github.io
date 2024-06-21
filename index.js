f4357c8dbf0959c6aaf2f5aad730d0241e8c41cc
Ireum0 <24-10711@gumigo.hs.kr>
2024-06-21 15:06:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ questions.forEach(function(question) {
         latestQuestionsSection.querySelector('ul').appendChild(li);
     }
 });
+
+// 질문 삭제 기능
+function deleteQuestion(index) {
+    // 로컬 스토리지에서 질문 데이터 가져오기
+    var questions = JSON.parse(localStorage.getItem('questions')) || [];
+
+    // 선택한 질문 삭제
+    questions.splice(index, 1);
+
+    // 업데이트된 질문 데이터를 로컬 스토리지에 저장
+    localStorage.setItem('questions', JSON.stringify(questions));
+
+    // 페이지 새로고침
+    window.location.reload();
+}
